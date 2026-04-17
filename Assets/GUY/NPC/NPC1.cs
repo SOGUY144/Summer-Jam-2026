d03474a3187281917b10d24803311487e1712e10
SOGUY <guyy9980@gmail.com>
2026-04-17 15:32:08 +0700

    ระบบพูดกับตัวละคร

---
+++ Assets/GUY/NPC/NPC1.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC1 : MonoBehaviour, IInteractable
+{
+    
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+    private bool isPlayerInRange;
+
+    private void Update()
+    {
+        // When the player presses 'F', trigger interaction
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isPlayerInRange || isDialogueActive)
+            {
+                Interact();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            
+            // Dialogue will start IMMEDIATELY when walking close
+            if (!isDialogueActive)
+            {
+                Interact();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            if (isDialogueActive)
+            {
+                EndDialogue();
+            }
+        }
+    }
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        // If no dialogue data or the game is paused and no dialogue is active
+        if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
+            return;
+
+        if (isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+        PauseController.SetPause(true);
+
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+
+        }
+        else if (dialogueIndex + 1 < dialogueData.dialogueLines.Length)
+        {
+            dialogueIndex++;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+        
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSecondsRealtime(dialogueData.typingSpeed);
+
+        }
+        
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSecondsRealtime(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+        PauseController.SetPause(false);    
+    }
+}
--- Assets/GUY/NPC/NPCDialogue.cs
+++ Assets/GUY/NPC/NPCDialogue.cs
@@ -6,10 +6,11 @@ public class NPCDialogue : ScriptableObject
     public string npcName;
     public Sprite npcPortrait;
     public string[] dialogueLines;
+    public bool[] autoProgressLines;
+    public float autoProgressDelay = 1.5f;
     public float typingSpeed;
     public AudioClip voiceSound;
     public float voicePitch = 1f;
-    public bool[] autoProgressLines;
-    public float autoProgressDelay = 1.5f;
+    
 
 }
