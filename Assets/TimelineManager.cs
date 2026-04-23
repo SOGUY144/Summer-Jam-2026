53a3384790bf7151608dedec9fe5bbb19b3b3ed3
Shokun234 <shokun.punyapat@gmail.com>
2026-04-23 16:53:21 +0700

    Timeline Turtorial done

--- Assets/TimelineManager.cs
+++ Assets/TimelineManager.cs
@@ -6,13 +6,15 @@ public class TimelineManager : MonoBehaviour
 
     public List<GameObject> enemiesInScene = new List<GameObject>();
     public GameObject Timeline2;
+    public bool IsStartTimeline1 = false;
 
     public void SpawnEnemy()
     {
         foreach (GameObject enemy in enemiesInScene)
         {
             enemy.SetActive(true);
-        }   
+        }
+        IsStartTimeline1 = true;
     }
     void CheckEnemies()
     {
@@ -23,7 +25,15 @@ public class TimelineManager : MonoBehaviour
         if (enemiesInScene.Count == 0 && enemiesInScene != null)
         {
             Timeline2.SetActive(true);
+            IsStartTimeline1 = false;
         }
 
+    }
+    void Update()
+    {
+        if (IsStartTimeline1)
+        {
+            CheckEnemies();
+        }
     }
 }
---
+++ Assets/TimelineManager2.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TimelineManager2 : MonoBehaviour
+{
+    public void loadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("View");
+    }
+}
